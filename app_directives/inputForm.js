d629279b0fca3fed6d60b7df1996dcba1064052e
Sriram <ssriram83@gmail.com>
2016-01-07 19:39:58 +0530

    7 Jan Merged Changes

--- app_directives/inputForm.js
+++ app_directives/inputForm.js
@@ -1,48 +1,32 @@
-// Not using this anymore..
-angular.forEach({
-    'input-text': 'appInputTextComponent',
-    'input-url': 'appInputUrlComponent',
-    'input-email': 'appInputEmailComponent',
-    'input-date': 'appInputDateComponent',
-    'select': 'appSelectComponent',
-    'textarea': 'appTextareaComponent',
-    'input-number': 'appInputNumberComponent',
-    'input-checkbox': 'appInputCheckboxComponent'
-}, function (directiveSelector, tpl) {
-    app
-        .directive(directiveSelector, ['$compile', 'ngForceConfig', function ($compile, ngForceConfig) {
-            return {
-                controller: 'FormFieldsCtrl',
-                controllerAs: 'ctrl',
-                templateUrl: ngForceConfig.resourceUrl + '/app_templates/field-templates/' + tpl + '.html',
-                scope: {
-                    model: '=',
-                    question: '='
-                }, link: function (scope, elem, attr) {
-                    if (scope.question.DataType__c == 'date' && angular.isDefined(scope.model)) {
-                        scope.model = new Date(scope.model);
-                    }
-                }
+app.directive("inputForm", function (ngForceConfig) {
+    return {
+        templateUrl: ngForceConfig.resourceUrl + '/app_templates/input-field.html',
+        scope: {
+            model: '=',
+            question: '='
+        }, link: function (scope, elem, attr) {
+            if (scope.question.DataType__c == 'date' && angular.isDefined(scope.model)) {
+                scope.model = new Date(scope.model);
             }
-        }])
-});
+        }
+    }
+})
 
-app.directive("showHideControl", function (ngForceConfig) {
+app.directive("showEditControl", function (ngForceConfig) {
     return {
         restrict: "E",
-        templateUrl: ngForceConfig.resourceUrl +"/app_templates/editControlButtons.html",
+        templateUrl: ngForceConfig.resourceUrl + "/app_templates/editControlButtons.html",
         scope: {},
         require: "^form",
         link: function (scope, element, attrs, form) {
             scope.form = form; //save parent form
             scope.searchButtonText = "Update"
-        }, controller:  'FormFieldsCtrl'
+        }, controller: 'FormsValidationCtrl'
 
     }
 })
 
-// Tying this to parent as it is needed to access the update fuctions..
-app.controller("FormFieldsCtrl", ['$scope', '$attrs', function ($scope, $attrs) {
+app.controller("FormsValidationCtrl", ['$scope', '$attrs', function ($scope, $attrs) {
     $scope.searchButtonText = "Update";
     $scope.$watch('model', function (newValue, oldValue) {
         //console.log('scope reached child : '+$scope.model);
